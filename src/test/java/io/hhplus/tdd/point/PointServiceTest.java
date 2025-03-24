da0217cd570fda001e0e9a25dedb3808e78a09fc
khs2973 <h1372@naver.com>
2025-03-25 00:43:46 +0900

    TEST: 기본 테스트 코드 추가

---
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -0,0 +1,19 @@
+package io.hhplus.tdd.point;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+public class PointServiceTest {
+
+	
+}
